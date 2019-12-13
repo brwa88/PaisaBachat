0acd28cccaf18464d1c43bc0c32bb682431fe3f1
ShyamVandan <56221868+ShyamVandan@users.noreply.github.com>
2019-12-13 10:57:00 +0100

    Add files via upload

---
+++ Documents/API/rest-api-authentication-example/api/ItemSelectConsumeIndexElectronics.php
@@ -0,0 +1,77 @@
+<?php
+
+
+
+
+
+
+$url = "http://localhost/PaiseBachat/APIs/ItemSelectIndexElectronics.php";
+
+ $client = curl_init($url);
+ curl_setopt($client,CURLOPT_RETURNTRANSFER,true);
+ $response = curl_exec($client);
+
+  $response=str_replace("}","},",$response);
+  $response=substr_replace($response,"",-1);
+$response="[".$response."]";
+ $result= json_decode($response);
+
+
+
+foreach($result as $result) {
+
+$name=$result->Item_Name;
+$price=$result->Price;
+$Catagory_Fkey=$result->Catagory_Fkey;
+
+if($Catagory_Fkey==2)
+{
+echo '		<div class="product">
+							<div class="product_image"><img src="images/product_2.jpg" alt=""></div>
+							<div class="product_extra product_new"><a href="categories.html">New</a></div>
+							<div class="product_content">
+								<div class="product_title"><a href="product.php">'.$name.'</a></div>
+								<div class="product_price">$'.$price.'</div>
+							</div>
+						</div>';
+}
+if($Catagory_Fkey==6)
+{
+echo '		<div class="product">
+							<div class="product_image"><img src="images/product_1.jpg" alt=""></div>
+							<div class="product_extra product_new"><a href="categories.html">New</a></div>
+							<div class="product_content">
+								<div class="product_title"><a href="product.php">'.$name.'</a></div>
+								<div class="product_price">$'.$price.'</div>
+							</div>
+						</div>';
+}
+
+/*
+    echo $result->Item_ID;
+
+
+
+    echo $result->Item_Name;
+
+
+
+    echo $result->Catagory_Fkey;
+
+    echo $result->Price;
+
+    echo $result->Sold;
+
+    echo $result->Post_Date;
+ echo $result->Images;
+    echo $result->Description;
+
+    echo $result->Verified;
+
+
+
+*/
+}
+
+
+    ?>
---
+++ Documents/API/rest-api-authentication-example/api/ItemSelectIndexBook.php
@@ -0,0 +1,59 @@
+<?php
+header("Content-Type:application/json");
+
+ include('../conn.php');
+
+
+
+
+
+
+$query="SELECT * FROM items where Verified=1 AND Catagory_Fkey=2 order by Item_ID DESC;";
+
+
+ $result=mysql_query($query);
+$jsonText="";
+
+ while($row = mysql_fetch_array($result))
+ {
+ $Item_ID = $row['Item_ID'];
+ $Item_Name = $row['Item_Name'];
+ $Catagory_Fkey = $row['Catagory_Fkey'];
+
+
+ $Price = $row['Price'];
+ $Sold = $row['Sold'];
+ $Post_Date = $row['Post_Date'];
+
+  $Images = $row['Images'];
+  $Description = $row['Description'];
+  $Verified = $row['Verified'];
+
+
+response($Item_ID, $Item_Name, $Catagory_Fkey,$Price, $Sold, $Post_Date,$Images, $Description, $Verified);
+
+
+ }
+ mysql_close($conn);
+
+
+
+function response($Item_ID, $Item_Name, $Catagory_Fkey,$Price, $Sold, $Post_Date,$Images, $Description, $Verified)
+{
+
+
+ $response['Item_ID']=$Item_ID;
+  $response['Item_Name']=$Item_Name;
+   $response['Catagory_Fkey']=$Catagory_Fkey;
+    $response['Price']=$Price;
+     $response['Sold']=$Sold;
+      $response['Post_Date']=$Post_Date;
+       $response['Description']=$Description;
+        $response['Images']=$Images;
+       $response['Verified']=$Verified;
+
+
+ $json_response = json_encode($response);
+echo $json_response;
+}
+?>
